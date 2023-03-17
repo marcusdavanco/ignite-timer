c992dd4e809d0dd113244e797fd3a1311d511904
Marcus Davanço <md.daves@gmail.com>
2023-03-17 03:10:36 -0300

    feat: Tipagem de temas

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,13 +4,6 @@ interface ButtonProps {
   variant?: ButtonVariant;
 }
 
-const buttonVariants = {
-  primary: "purple",
-  secondary: "orange",
-  danger: "red",
-  success: "green",
-};
-
 export function Button({ variant = "primary" }: ButtonProps) {
   return <ButtonContainer variant={variant}>Enviar</ButtonContainer>;
 }
