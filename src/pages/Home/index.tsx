7c249721412f6027aaebffa450d79463b98b2243
Marcus Davanço <md.daves@gmail.com>
2023-03-22 17:29:57 -0300

    feat: Criando reducer de ciclos

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -38,6 +38,7 @@ export function Home() {
   const { handleSubmit, watch, reset } = newCycleForm
 
   function handleCreateNewCycle(data: NewCycleFormData) {
+    console.log(createNewCycle)
     createNewCycle(data)
     reset()
   }
